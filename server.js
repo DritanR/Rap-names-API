d73b2d0ce709d6f312284dad54e2155f5f009526
Dritan <dritanrexhepi04@gmail.com>
2022-11-28 10:01:46 -0800

    Did some changes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require ('express')
 const app = express()
+const cors = require('cors')
 const PORT = 8000
+
+app.use(cors())
 
 const rappers = {
     '21 savage': {
@@ -33,6 +36,6 @@ app.get('/api/:name', (request, response) => {
     }
 })
 
-app.listen(PORT, () => {
-    console.log(`The server is now running on ${PORT}. You better go catch it!`)
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`The server is now running. You better go catch it!`)
 })
